56248efe3f161414bd88847210450d6e7c38d153
yaroslav.matushevych@innovecs.com <yaroslav.matushevych@innovecs.com>
2020-01-16 21:12:37 +0200

    share results of search by url

--- src/containers/StarShips/StarShips.tsx
+++ src/containers/StarShips/StarShips.tsx
@@ -1,6 +1,8 @@
 // modules
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import { connect } from 'react-redux';
+import QS from 'query-string';
+import { useHistory } from 'react-router-dom';
 // components
 import ShipItems from './components/ShipItems';
 import Button from '../../ui-library/components/Button';
@@ -22,33 +24,33 @@ type Props = {
   fetchData: (url: RequestInfo) => {};
 }
 
-const StarShips: React.FC<Props & ReturnType<typeof mapStateToProps>> = ({
+const StarShips: React.FC<Props & ReturnType<typeof mapStateToProps>> = memo(({
   items,
   hasErrored,
   isLoading,
   fetchData,
 }) => {
-
-  const [name, setName] = useState('');
+  const { location, push } = useHistory();
+  const { name = '' } = QS.parse(location.search);
   const [pageNumber, setPageNumber] = useState(1);
 
   const searchByName = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setName(e.target.value);
+    push(`${location.pathname}?name=${e.target.value}`);
   };
 
   useEffect(() => {
-    fetchData('https://swapi.co/api/starships/');
-  }, []);
+    fetchData(`https://swapi.co/api/starships/?search=${name}`);
+  }, [fetchData, name]);
 
   useEffect(() => {
-    fetchData(`https://swapi.co/api/starships/?search=${name}`);
-  }, [name]);
+    fetchData('https://swapi.co/api/starships/');
+  }, [fetchData]);
+
 
   useEffect(() => {
     fetchData(`https://swapi.co/api/starships/?page=${pageNumber}`)
-  }, [pageNumber]);
+  }, [pageNumber, fetchData]);
 
-  console.log(items);
   const setPageNumberWrapper = (e: React.MouseEvent<HTMLButtonElement>) => {
     const target = e.target as HTMLTextAreaElement;
     (target.id === 'next' && items.next !== null) && setPageNumber(pageNumber + 1);
@@ -57,15 +59,16 @@ const StarShips: React.FC<Props & ReturnType<typeof mapStateToProps>> = ({
 
   return (
     <main className={styles.mainBlock}>
+
       <h2 className={styles.heading}>List of our Imperial Space Fleet</h2>
 
-      <input 
-        className={styles.searchInput} 
-        type="text" 
-        placeholder='Search By Name' 
-        name='name' 
-        onChange={searchByName} 
-        value={name} 
+      <input
+        className={styles.searchInput}
+        type="text"
+        placeholder='Search By Name'
+        name='name'
+        onChange={searchByName}
+        value={name || ''}
       />
 
       <div className={styles.shipsContainer}>
@@ -93,7 +96,7 @@ const StarShips: React.FC<Props & ReturnType<typeof mapStateToProps>> = ({
         />}
     </main>
   );
-};
+});
 
 const mapStateToProps = (state: AppState) => {
   return {
